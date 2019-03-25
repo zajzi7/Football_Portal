828184c4f9708c4db46359250aa3ad0a62f5c4af
zajzi7 <dominix77@rocketmail.com>
2019-03-26 00:43:12 +0100

    Initial

---
+++ src/test/java/pl/dominik/football/FootballApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.dominik.football;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FootballApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
