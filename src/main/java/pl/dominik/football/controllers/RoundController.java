63b679b666017a8d5758baa929e6193d10959894
zajzi7 <dominix77@rocketmail.com>
2019-05-18 19:33:10 +0200

    Round validation added.

--- src/main/java/pl/dominik/football/controllers/RoundController.java
+++ src/main/java/pl/dominik/football/controllers/RoundController.java
@@ -1,8 +1,12 @@
 package pl.dominik.football.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +14,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import pl.dominik.football.domain.entity.Round;
 import pl.dominik.football.services.RoundService;
 
-import java.util.List;
+import javax.validation.Valid;
 
 @Controller
 public class RoundController {
+
+    //Auxiliary variable to get String value from validation.messages.properties and add to the error
+    @Value("${pl.round.validation.alreadyExistInSeason.message}")
+    String roundNumberAlreadyExists;
 
     @Autowired
     RoundService roundService;
@@ -28,17 +36,30 @@ public class RoundController {
 
     @RequestMapping(value = "/rounds", method = RequestMethod.POST)
     //Create new round in DB received from admin data and then redirect to round list
-    public String saveRound(@ModelAttribute("round") Round round, int seasonId) {
-        List<Round> rounds = roundService.getRoundsBySeasonId(seasonId);
+    public String saveRound(
+            @Valid @ModelAttribute("round") Round round,
+            BindingResult bindingResult,
+            int seasonId,
+            Model model) {
 
-        for (Round r : rounds) {
-            if (r.getRoundNumber() == round.getRoundNumber()) {
-                //loop to check if round number entered by admin already exists in this season
-                return "error-name";
-            }
+        //Check if roundNumber value entered by the admin contains errors(validation @Min, @Max in the Round class)
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("seasonId", seasonId);
+            return "create-round";
         }
 
-        roundService.assignRoundToSeasonWithId(round, seasonId);
+        try {
+            //Check if roundNumber value entered by the admin is already exists in this season
+            //(handle @UniqueConstraint in the Round class)
+            roundService.assignRoundToSeasonWithId(round, seasonId);
+        } catch (DataIntegrityViolationException e) {
+            ObjectError error = new ObjectError("roundNumber", roundNumberAlreadyExists);
+            bindingResult.addError(error);
+            model.addAttribute("seasonId", seasonId);
+            return "create-round";
+        }
+
+        //If everything went well then redirect to the rounds list
         return "redirect:/seasons/show-rounds/" + seasonId;
     }
 
--- src/main/java/pl/dominik/football/domain/entity/Round.java
+++ src/main/java/pl/dominik/football/domain/entity/Round.java
@@ -6,29 +6,38 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @NoArgsConstructor
 @ToString
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"round_number", "season_id"})})
 public class Round {
 
     //Class to store single rounds. Also known as matchday(all matches of the week; usually played in one day)
-
     @Id
     @Getter @Setter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     //Field to store round number in the current season(example: Round 1, season 2019/2020)
+    @Column(name = "round_number")
+    @Min(value = 1, message = "{pl.round.validation.min}")
+    @Max(value = 99, message = "{pl.round.validation.max}")
     @Getter @Setter
     private int roundNumber;
 
@@ -38,6 +47,7 @@ public class Round {
 
     @ManyToOne
     @Getter @Setter
+    @JoinColumn(name = "season_id")
     private Season season;
 
     public Round(int roundNumber) {
